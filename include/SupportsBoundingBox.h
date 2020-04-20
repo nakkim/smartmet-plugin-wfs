56b1a9c0afefb1b212b2829fa869571ec78724f0
Mika Heiskanen <mika.heiskanen@fmi.fi>
2020-04-20 14:43:37 +0300

    Interim

--- include/SupportsBoundingBox.h
+++ include/SupportsBoundingBox.h
@@ -4,7 +4,7 @@
 #include "StoredQueryParamRegistry.h"
 #include "SupportsExtraHandlerParams.h"
 #include <engines/gis/CRSRegistry.h>
-#include <gdal/ogr_geometry.h>
+#include <ogr_geometry.h>
 
 namespace SmartMet
 {
