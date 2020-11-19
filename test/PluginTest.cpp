24cd07421fea2d7335b86a13e35a89a021f68e08
Andris Pavenis <andris.pavenis@fmi.fi>
2020-11-19 12:33:10 +0200

    [Plugin tests] Test infrastructure update

--- test/PluginTest.cpp
+++ test/PluginTest.cpp
@@ -2,6 +2,7 @@
 #include <spine/PluginTest.h>
 #include <iostream>
 #include <sstream>
+#include <boost/program_options.hpp>
 
 using namespace std;
 
@@ -29,15 +30,55 @@ int main(int argc, char* argv[])
 {
   try
   {
+    namespace po = boost::program_options;
+
+    po::options_description desc("Allowed options");
+
+    const char* nm_config = "reactor-config";
+    const char* nm_input = "input-dir";
+    const char* nm_expected = "expected-output-dir";
+    const char* nm_failures = "failures-dir";
+    const char* nm_threads = "num-threads";
+
+    desc.add_options()
+        (nm_config, po::value<std::string>(), "Reactor config file")
+        (nm_input, po::value<std::string>(), "Tests input directory (default 'input')")
+        (nm_expected, po::value<std::string>(), "Expected output directory (default 'output')")
+        (nm_failures, po::value<std::string>(), "Directory where to write actual output in"
+            " case of failures (default value 'failures')")
+        (nm_threads, po::value<int>(), "Number of threads (default 10)")
+        ;
+
+    po::variables_map opt;
+    po::store(po::parse_command_line(argc, argv, desc), opt);
+
+    po::notify(opt);
+
     SmartMet::Spine::Options options;
-    options.configfile = argc == 1 ? "cnf/wfs_plugin_test.conf" : argv[1];
+    options.configfile = opt.count(nm_config)
+        ? opt[nm_config].as<std::string>()
+        : std::string("cnf/wfs_plugin_test.conf");
     options.quiet = true;
     options.defaultlogging = false;
     options.accesslogdir = "/tmp";
 
     try
     {
-      return SmartMet::Spine::PluginTest::test(options, prelude, false, 10);
+        SmartMet::Spine::PluginTest tester;
+        if (opt.count(nm_input)) {
+            tester.setInputDir(opt[nm_input].as<std::string>());
+        }
+        if (opt.count(nm_expected)) {
+            tester.setOutputDir(opt[nm_expected].as<std::string>());
+        }
+        if (opt.count(nm_failures)) {
+            tester.setFailDir(opt[nm_failures].as<std::string>());
+        }
+        if (opt.count(nm_threads)) {
+            tester.setNumberOfThreads(opt[nm_threads].as<int>());
+        }
+
+        return tester.run(options, prelude);
     }
     catch (const libconfig::ParseException& err)
     {
---
+++ test/tests.mk
@@ -0,0 +1,85 @@
+ifeq ($(TOP),)
+$(error "TOP no defined")
+endif
+
+ifeq ($(wildcard $(TOP)/cnf/observation_common.conf),)
+$(error "Cannot find configuration files")
+endif
+
+XML_TEST_SRC = $(shell find xml -name '*.xml')
+XML_POST_TESTS = $(subst :,\:,$(patsubst xml/%.xml, input/%.xml.post, $(XML_TEST_SRC)))
+KVP_TEST_SRC = $(shell find kvp -name '*.kvp')
+KVP_GET_TESTS = $(subst :,\:,$(patsubst kvp/%.kvp, input/%.kvp.get, $(KVP_TEST_SRC)))
+
+VALGRIND = valgrind \
+	--num-callers=30 \
+	--gen-suppressions=all \
+	--suppressions=../cnf/valgrind/valgrind_libclntsh.supp \
+	--suppressions=../cnf/valgrind/valgrind_brainstorm.supp
+
+foo:
+	echo $(XML_TEST_SRC)
+	echo $(XML_POST_TESTS)
+
+all:
+
+ifdef CI
+TEST_TARGETS := fminames test_sqlite
+EXTRA_IGNORE := .testignore-ci
+else
+TEST_TARGETS := test_sqlite test_oracle test_postgresql
+EXTRA_IGNORE :=
+endif
+
+test:	all
+	@ok=true; failed=; \
+	for test in $(TEST_TARGETS); do \
+	  if ! $(MAKE) $$test; then ok=false; failed="$$failed $$test"; fi; \
+        done; \
+	if ! $$ok ; then \
+	  echo "======================================================================="; \
+	  echo "Failed: $$failed"; \
+	  echo "======================================================================="; \
+        fi; \
+        $$ok
+
+clean:
+	rm -fv $(shell find input -type f -a -name '*.xml.post')
+	rm -rf failures-oracle failures-postgresql failures-sqlite
+	rm -vf cnd/wfs_plugin_test_*.conf
+
+test_oracle:		DB_TYPE=oracle
+test_postgresql:	DB_TYPE=postgresql
+test_sqlite:		DB_TYPE=sqlite
+
+fminames:
+	sed -i -e 's/"smartmet-test"/"localhost"/g' cnf/*.conf
+	@/usr/share/smartmet/test/db/init-and-start.sh && /usr/share/smartmet/test/db/install-test-db.sh > /dev/null
+
+test_oracle test_postgresql test_sqlite: ../PluginTest s-input-files ../cnf/local.conf
+	rm -rf failures-$(DB_TYPE)
+	mkdir -p failures-$(DB_TYPE)
+	cat $(wildcard input/.testignore-$(DB_TYPE)) $(EXTRA_IGNORE) /dev/null | sort | uniq >input/.testignore;
+	cat $(TOP)/cnf/wfs_plugin_test.conf.in | sed -e 's:@TARGET@:$(DB_TYPE):g' >cnf/wfs_plugin_test_$(DB_TYPE).conf
+	../PluginTest --reactor-config cnf/wfs_plugin_test_$(DB_TYPE).conf --failures-dir failures-$(DB_TYPE)
+
+s-input-files:
+	rm -f $(shell find input -name '*.xml.post' -o -name '*.kvp.get')
+	$(MAKE) $(XML_POST_TESTS)
+	$(MAKE) $(KVP_GET_TESTS)
+
+input/%.xml.post : xml/%.xml ; @mkdir -p $(shell dirname $@)
+	@perl ../MakeXMLPOST.pl $< $@ /wfs
+
+input/%.kvp.get : kvp/%.kvp ; @mkdir -p $(shell dirname $@)
+	@perl ../MakeKVPGET.pl $< $@ /wfs
+
+../cnf/local.conf:
+	@echo "***************************************************************************************************"
+	@echo "* Please copy file $(shell cd ..; pwd)/cnf/local.conf.in to"
+	@echo "* $(shell cd ..; pwd)/cnf/local.conf and edit required names in it"
+	@echo "***************************************************************************************************"
+	@false
+
+../PluginTest:
+	$(MAKE) -C .. PluginTest
